f6bb9ca9a20beb9d28bcd7b97c516a698e52ed63
HIChen <295098309@qq.com>
2017-04-28 11:07:41 +0800

    Initial Commit

---
+++ iOSAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSAnimation
+//
+//  Created by 陈汉楠 on 17/4/28.
+//  Copyright © 2017年 h_nChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
